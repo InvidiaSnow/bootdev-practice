7c39773bce2a58b6a8aac9b3226b36b87c249e52
Sølve <solve.sem@leakdetector.no>
2026-09-08 10:10:41 +0200

    main loads configs from .env

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()  # reads .env into environment variables, if the file exists
+
+API_TOKEN = os.environ["API_TOKEN"]
+IXON_EMAIL = os.environ["IXON_EMAIL"]
+
+# Debug
+print(IXON_EMAIL)
 
 def main():
-    print("Running bootdev-practice main.py")
+    print("Running dts-fleet-overview main.py")
 
 if __name__ == "__main__":
     main()
